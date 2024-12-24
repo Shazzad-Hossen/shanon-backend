9bf64b0a04c3c81e1ad3f54833686fb3980ffcc8
Shazzad-Hossen <tipsfire.com@gmail.com>
2024-12-25 02:51:40 +0600

    subcategory api auth middleware removed

--- src/services/subcategory/subcategory.js
+++ src/services/subcategory/subcategory.js
@@ -5,7 +5,7 @@ const {  createSubCategory, getAllSubCategory, deleteSubCategory, editSubCategor
 function subCategory(){
     //create new category
     this.route.post('/sub-category', auth, checkRole(['super-admin']),createSubCategory(this));
-    this.route.get('/sub-category', auth,getAllSubCategory(this));
+    this.route.get('/sub-category',getAllSubCategory(this));
     this.route.get('/sub-category/:_id', auth, checkRole(['super-admin']),getSingleSubCategory(this));
     this.route.delete('/sub-category/:_id', auth, checkRole(['super-admin']),deleteSubCategory(this));
     this.route.patch('/sub-category/:_id', auth, checkRole(['super-admin']),editSubCategory(this));
