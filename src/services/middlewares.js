488e7a2bc17e0afccfb877e90933ca9c9ac84055
Shazzad-Hossen <tipsfire.com@gmail.com>
2025-01-11 05:30:59 +0600

    auth middleware updated

--- src/services/middlewares.js
+++ src/services/middlewares.js
@@ -4,18 +4,22 @@ const User = require('./user/user.schema')
 
 
 module.exports.auth= async(req,res,next)=>{
-    let cookie = req.cookies[settings.COOKIE_NAME];
-    if(!cookie){
-        const authorization = req.headers.authorization;
-        if(authorization) cookie=authorization.split(' ')[1];
-    }
-    if(!cookie)return res.status(401).send({ message: 'Unauthorized' });
-    const decoded =  crypto.decrypt(cookie);
-    if(!decoded._id || !decoded.email) return res.status(401).send({ message: 'Unauthorized' });
+    try {
+    const authorization = req?.headers?.authorization;
+    if(!authorization) return res.status(401).send({ message: 'Unauthorized' });
+    const token=authorization.split(' ')[1];    
+    if(!token) return res.status(401).send({ message: 'Unauthorized' });
+    const decoded =  crypto.decrypt(token);
+    if(!decoded?._id || !decoded?.email) return res.status(401).send({ message: 'Unauthorized' });
     const user = await User.findOne({_id:decoded._id});
-    if(!user)return res.status(401).send({ message: 'Unauthorized' });
+    if(!user) return res.status(401).send({ message: 'Unauthorized' });
     else req.user=user;
     next();
+        
+    } catch (error) {
+        return res.status(500).send({ message: 'Something went wrong' });
+        
+    }
 };
 
 
